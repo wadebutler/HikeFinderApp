7335d30960d790733b79b60533a8135aef820014
Wade Butler <wadebutler2@gmail.com>
2020-08-24 18:33:09 -0400

    cleaned up code

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,21 +1,29 @@
 import React, {useState} from 'react';
-import {Input, Button} from 'react-native-elements';
+import {Input, Button, Text} from 'react-native-elements';
 import Axios from 'axios';
 
 const Search = () => {
     const [city, setCity] = useState('');
-    const apikey = "AIzaSyCZU67m9SoARSn7gcWUR2g6Ke8Ze4pWMNQ";
+    const [lat, setLat] = useState('');
+    const [lng, setLng] = useState('');
+    const geoApi = "AIzaSyCZU67m9SoARSn7gcWUR2g6Ke8Ze4pWMNQ";
 
     const cityToLatLong = () => {
         Axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
             params: {
                 address: city,
-                key: apikey,
+                key: geoApi,
             }
         }).then((results) => {
             const lat = results.data.results[0].geometry.location.lat
             const lng = results.data.results[0].geometry.location.lng
+            setLat(lat)
+            setLng(lng)
         })
+    }
+
+    const findHike = () => {
+        Axios.get("")
     }
     return (
         <>
